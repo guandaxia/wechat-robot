61207c1ec840b9206165813be24fa9fce7f9152a
guansixu <guansixu@gmail.com>
2018-02-12 17:39:18 +0800

    注释自行车

--- CustromHandler.php
+++ CustromHandler.php
@@ -10,6 +10,7 @@ namespace Guandaxia;
 
 use Guandaxia\Handlers\Cron\Bike;
 use Guandaxia\Handlers\Cron\DaliyReport;
+// use Guandaxia\Handlers\Cron\Express;
 use Guandaxia\Handlers\Cron\Weather;
 
 class CustromHandler
@@ -17,10 +18,12 @@ class CustromHandler
     public static function custromHandler()
     {
         //自行车
-        Bike::cronHandler();
+       // Bike::cronHandler();
         //天气
         Weather::cronHandler();
         //日报
         //DaliyReport::cronHandler();
+        //快递
+//        Express::cronHandler();
     }
 }
