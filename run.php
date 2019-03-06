f064c1f28a80cae0481b40773df8ccc7a84b911d
guansixu <guansixu@gmail.com>
2019-03-06 17:16:23 +0800

    增加数据库处理方法

--- run.php
+++ run.php
@@ -2,11 +2,35 @@
 
 namespace Guandaxia;
 
+use think\Db;
+
 require __DIR__ . '/Loader.php';
 //注册自动加载
 Loader::register();
 
 $vbot = new VbotHandler();
+
+Db::setConfig([
+    // 数据库类型
+    'type' => 'mysql',
+    // 服务器地址
+    'hostname' => '127.0.0.1',
+    // 数据库名
+    'database' => 'vbot',
+    // 数据库用户名
+    'username' => 'homestead',
+    // 数据库密码
+    'password' => 'secret',
+    // 数据库连接端口
+    'hostport' => '33060',
+    // 数据库连接参数
+    'params' => [],
+    // 数据库编码默认采用utf8
+    'charset' => 'utf8',
+
+    'debug'           => true,
+]);
+
 $vbot->run();
 
 
